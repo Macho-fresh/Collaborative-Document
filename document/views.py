75337ca843c785b8f8592a57a47f5e466428df49
Macho-Fresh <chycyber13@gmail.com>
2026-09-09 09:37:20 +0100

    views

--- document/views.py
+++ document/views.py
@@ -4,5 +4,75 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import *
 from accounts.models import User
+from rest_framework.permissions import IsAuthenticated
+from .serializers import *
 
-# class CreateDoc(APIView)
+class CreateDoc(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        user_id = request.user.id
+        title = request.data.get('title')
+        content = request.data.get('content')
+        user = User.objects.get(id=user_id)
+
+        Document.objects.create(
+            user = user,
+            title = title,
+            content = content,
+            version = 1
+        )
+
+        return Response({
+            'message': 'Document created'
+        },status=status.HTTP_201_CREATED)
+
+
+class GetAllDoc(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        doc = Document.objects.all()
+        serializer = DocSerializer(doc)
+        return Response({
+            serializer.data
+        },status=status.HTTP_200_OK)
+
+class GetDoc(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request, id):
+        doc = Document.objects.get(id=id)
+        serializer = DocSerializer(doc)
+        return Response({
+            serializer.data
+        },status=status.HTTP_200_OK)
+
+class PatchDoc(APIView):
+    permission_classes = [IsAuthenticated]
+    def patch(self, request, id):
+        user_id = request.user.id
+        title = request.data.get('title')
+        content = request.data.get('content')
+        user = User.objects.get(id=user_id)
+
+        doc = Document.objects.get(id=id)
+        if doc.user != user:
+            return Response({
+                'error': 'not document owner'
+            },status=status.HTTP_401_UNAUTHORIZED)
+        doc.title = title
+        doc.content = content
+        doc.save()
+        serializer = DocSerializer(doc)
+
+        return Response({
+            serializer.data
+        },status=status.HTTP_202_ACCEPTED)
+
+class DeleteDoc(APIView):
+    permission_classes = [IsAuthenticated]
+    def delete(self, request, id):
+        doc = Document.objects.get(id=id)
+        doc.delete()
+
+        return Response({
+            'message': 'doc deleted'
+        },status=status.HTTP_200_OK)
