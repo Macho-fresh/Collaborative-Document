c55e74268ca012dc91debf0adfb4ad6aad27227e
Macho-Fresh <chycyber13@gmail.com>
2026-09-10 12:57:58 +0100

    edited permissions

--- document/permissions.py
+++ document/permissions.py
@@ -5,12 +5,12 @@ class ViewerPermission(BasePermission):
     def has_permission(self, request, view):
         doc_id = view.kwargs.get('id')
         doc = Document.objects.get(id=doc_id)
-        if request.user.id in doc.viewers:
+        if request.user.id in doc.viewers and request.user.is_authenticated:
             return True
 
 class EditorPermission(BasePermission):
     def has_permission(self, request, view):
         doc_id = view.kwargs.get('id')
         doc = Document.objects.get(id=doc_id)
-        if request.user.id in doc.editors:
+        if request.user.id in doc.editors and request.user.is_authenticated:
             return True
