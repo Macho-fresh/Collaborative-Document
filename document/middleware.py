2fe265015af3f85c121cfc101df879c053e0b0c6
Macho-Fresh <chycyber13@gmail.com>
2026-09-13 21:55:52 +0100

    removed middleware

--- document/middleware.py
+++ document/middleware.py
@@ -1,12 +1,13 @@
-from .models import AuditLog
-from accounts.models import User
+# from .models import AuditLog
+# from accounts.models import User
 
-class Middleware():
-    def __init__(self, get_response):
-        self.get_response = get_response
+# class Middleware():
+#     def __init__(self, get_response):
+#         self.get_response = get_response
 
-    def __call__(self, request):
-        AuditLog.objects.create(
-            
-        )
-        return self.get_response(request)
+#     def __call__(self, request):
+#         if request.path == 
+#         AuditLog.objects.create(
+
+#         )
+#         return self.get_response(request)
