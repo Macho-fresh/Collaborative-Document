5369a124bce8524ce8bc0a0eb40c2c8221d445bc
Macho-Fresh <chycyber13@gmail.com>
2026-09-18 18:17:24 +0100

    routing for ws

--- project/asgi.py
+++ project/asgi.py
@@ -1,10 +1,21 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
+
+  "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                path("chat/admin/", AdminChatConsumer.as_asgi()),
+            ])
+        )
+    ),
 })
